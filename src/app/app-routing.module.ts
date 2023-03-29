1534f5e474213390cca7d8578c18e77b0b9a1c2a
nam <nobeltia@gmail.com>
2023-03-29 10:54:15 +0700

    complete routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FournotfourComponent } from './fournotfour/fournotfour.component';
 import { HomeComponent } from './home/home.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { SinglePostComponent } from './single-post/single-post.component';
@@ -8,12 +9,11 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'posts', component: PostListComponent },
   { path: 'post/:id/:title', component: SinglePostComponent },
-]
+  { path: '**', component: FournotfourComponent },
+];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { PostListComponent } from './post-list/post-list.component';
 import { HomeComponent } from './home/home.component';
 import { SinglePostComponent } from './single-post/single-post.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FournotfourComponent } from './fournotfour/fournotfour.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
     HomeComponent,
-    SinglePostComponent
+    SinglePostComponent,
+    FournotfourComponent
   ],
   imports: [
     BrowserModule,
